f4c8e2eca7932a50a4d85800f39c243322e4efbb
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-02-12 00:31:20 +0530

    Small changes

--- survey/admin.py
+++ survey/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from survey.models import FormId, OptionType, Question, Option
 
-# Register your models here.
+admin.site.register(FormId)
+admin.site.register(OptionType)
+admin.site.register(Question)
+admin.site.register(Option)
--- survey/models.py
+++ survey/models.py
@@ -7,15 +7,23 @@ from django.db import models
 
 class FormId(models.Model):
     form_id = models.AutoField(primary_key=True)
+    def __str__(self):
+        return str(self.form_id)
 
 class OptionType(models.Model):
     option_type = models.CharField(max_length=255, unique=True)
+    def __str__(self):
+        return self.option_type
 
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.TextField(blank=True, default="")
     option_type = models.ForeignKey(OptionType)
     form_id = models.ForeignKey(FormId, related_name = "form_questions")
+    def __str__(self):
+        return str(question_text) + str(option_type)
 
-class Options(models.Model):
+class Option(models.Model):
     option_text = models.CharField(max_length=255, unique=True)
-    question_id = models.ForeignKey(Questions)
+    question_id = models.ForeignKey(Question)
+    def __str__(self):
+        return str(option_text)
