a0619e42174e80978443ebbf944fb78e456c55c5
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-02-12 00:11:45 +0530

    Database created - models added

--- survey/models.py
+++ survey/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class FormId(models.Model):
+    form_id = models.AutoField(primary_key=True)
+
+class OptionType(models.Model):
+    option_type = models.CharField(max_length=255, unique=True)
+
+class Questions(models.Model):
+    question_text = models.TextField(blank=True, default="")
+    option_type = models.ForeignKey(OptionType)
+    form_id = models.ForeignKey(FormId, related_name = "form_questions")
+
+class Options(models.Model):
+    option_text = models.CharField(max_length=255, unique=True)
+    question_id = models.ForeignKey(Questions)
