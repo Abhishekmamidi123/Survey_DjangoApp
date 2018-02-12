e85ee44de2b9141062bb8b70abed1400e91d862c
Abhishekmamidi123 <abhishek.m15@iiits.in>
2018-02-12 16:13:05 +0530

    Take survey - 50% done

--- survey/views.py
+++ survey/views.py
@@ -8,8 +8,19 @@ def createSurvey(request):
     return render(request, 'survey/create_survey.html', None)
 
 def takeSurvey(request):
-    pk = 1
+    form = []
+    pk = 2
     questions_list = Question.objects.filter(form_id = pk)
-    # for question in questions_list:
-
-    return render(request, 'survey/take_survey.html', None)
+    options_list = Option.objects.all()
+    for question in questions_list:
+        Question_list = {}
+        Question_list['question_text'] = question.question_text
+        Question_list['option_type'] = question.option_type.option_type
+        options = options_list.filter(question_id = question.pk)
+        list_of_options = []
+        for option in options:
+            list_of_options.append(option.option_text)
+        Question_list['options'] = list_of_options
+        form.append(Question_list)
+    context={'form':form}
+    return render(request, 'survey/take_survey.html', context)
